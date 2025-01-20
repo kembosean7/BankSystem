972961a80d97936111e21cc53732e2f58068b01c
Sean Kembo <kembosean7@gmail.com>
2025-01-20 22:35:49 +0200

    mapping my User class to my database

--- demo/src/main/java/com/example/demo/UserController.java
+++ demo/src/main/java/com/example/demo/UserController.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import java.util.List;
 @RequestMapping(path = "api/bank")
 public class UserController {
 
+    @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
     }
